56b38641bf49d46426d7f73a14babf9275a71b01
Derrick Cudd <derrickc@videoideas.com>
2018-10-11 12:53:08 -0400

    Clean up and comment code

--- public/services/events.js
+++ public/services/events.js
@@ -1,27 +1,30 @@
 const { ipcMain } = require('electron');
 const { exec } = require('child_process');
-// const osascript = require('osascript');
 
-const { on } = ipcMain;
 const registerEvents = () => {
+  // Open project in favorite editor
+  // @TODO Add ability to change editor preference
+  // Currently hardcoded to use code-insiders
   ipcMain.on('open-editor', (_, project) => {
     exec(`code-insiders "${project.path}/"`, (_, out) => {
       console.log('launching in VSCode...');
     });
   });
 
+  // Open project directory in Finder
   ipcMain.on('open-in-finder', (_, project) => {
     exec(`open ${project.path}/`, (_, out) => {
       console.log('Launching in Finder...');
     });
   });
 
+  // Open new terminal window at the project's root
+  // @TODO ALlow for a choice of terminal application
   ipcMain.on('open-terminal', (_, project) => {
-    // Escape spaces
+    // Double-escape spaces in path
     const path = project.path.replace(/(\s+)/g, '\\\\$1');
 
-    // Open a new at the project's path
-    // @TODO ALlow for a choice of terminal application
+    // Applescript that opens a new terminal window and CDs into the project's root directory
     const openTerminalAtPath = `
       tell application "iTerm2"
         set newWindow to (create window with default profile)
@@ -31,12 +34,16 @@ const registerEvents = () => {
       end tell
     `;
 
-    console.log('launching terminal', path);
-
+    // Run the Applescript
     osascript(openTerminalAtPath);
   });
+
+  // @TODO Run a script in package.json
+  ipcMain.on('run-script', (_, { project, script }) => {});
 };
 
+// Execute some Applescript
+// This was the best way I could find to manage terminal windows.
 // @TODO Move into a separate file
 const osascript = script => {
   exec(`osascript -e '${script}'`, (error, stdout, stderr) => {
